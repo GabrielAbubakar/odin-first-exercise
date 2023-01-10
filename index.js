dc29a80dc146d3b6e4fcea5e8adba7aa5c2094b1
Gabriel Abubakar <gabrielogrima@gmail.com>
2023-01-10 01:47:21 +0100

    same application using express js

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+
+const app = express()
 
 
-var server = http.createServer(function (req, res) {
-    if (req.url == '/') {
 
-        fs.readFile('./index.html', function (err, data) {
-            if (err) {
-                res.writeHead(404)
-                res.write("Contents not found")
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' })
-                res.write(data)
-                return res.end()
-            }
-        })
-
-    } else if (req.url == '/about') {
-
-        fs.readFile('./about.html', function (err, data) {
-            if (err) {
-                res.writeHead(404)
-                res.write("Contents not found")
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' })
-                res.write(data)
-                return res.end()
-            }
-        })
-
-    } else if (req.url == '/contact-me') {
-
-        fs.readFile('./contact-me.html', function (err, data) {
-            if (err) {
-                res.writeHead(404)
-                res.write("Contents not found")
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' })
-                res.write(data)
-                return res.end()
-            }
-        })
-
-    }
-
-    else {
-        fs.readFile('./404.html', function (err, data) {
+app.get('/', (req, res) => {
+    fs.readFile('./index.html', function (err, data) {
+        if (err) {
             res.writeHead(404)
+            res.write("Contents not found")
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
             res.write(data)
             return res.end()
-        })
-    }
+        }
+    })
 })
 
-server.listen(8080)
+app.get('/about', (req, res) => {
+    fs.readFile('./about.html', function (err, data) {
+        if (err) {
+            res.writeHead(404)
+            res.write("Contents not found")
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.write(data)
+            return res.end()
+        }
+    })
+})
+
+app.get('/contact-me', (req, res) => {
+    fs.readFile('./contact-me.html', function (err, data) {
+        if (err) {
+            res.writeHead(404)
+            res.write("Contents not found")
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.write(data)
+            return res.end()
+        }
+    })
+})
+
+app.get('*', (req, res) => {
+    fs.readFile('./404.html', function (err, data) {
+        res.writeHead(404)
+        res.write(data)
+        return res.end()
+    })
+})
+
+app.listen(8080)
 
 console.log('server started')
